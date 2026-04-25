42d44b5650ceb384d7986a53957d09934cb7f74b
sarasalgueron <a2025116446@estudiantes.upsa.edu.bo>
2026-04-24 22:45:09 -0400

    Create Dato.h

---
+++ Dato.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <iostream>
+using namespace std;
+class Dato {
+public:
+    double Codigo;
+    string Nombre;
+    string Carrera;
+    Dato() {
+        Codigo = 0;
+        Nombre = "Unknow";
+        Carrera = "Unknow";
+    }
+};
